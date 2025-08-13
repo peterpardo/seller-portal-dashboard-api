9f7134f26babb0d101fbe3b42afb376d060c1b82
Peter Pardo <peterpardo123@gmail.com>
2025-08-14 00:22:50 +0800

    Export global prisma object

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
+export const prisma =
   globalForPrisma?.prisma ??
   new PrismaClient({ log: ["query", "error", "warn"] });
 
