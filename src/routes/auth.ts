0238b44ee7a03d923fba316a73f6b1924d9ed553
Peter Pardo <peterpardo123@gmail.com>
2025-08-19 22:45:40 +0800

    Added login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { prisma } from "../lib/prisma";
-import { genSalt, hash } from "bcryptjs";
-import { generateSlug, generateUniqueStoreSlug } from "../utils/slug";
+import { compare, genSalt, hash } from "bcryptjs";
+import { generateUniqueStoreSlug } from "../utils/slug";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS as string);
@@ -45,6 +46,41 @@ router.post("/register", async (req, res) => {
     });
 
     res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+// POST /api/auth/login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+      include: { store: true },
+    });
+
+    // check if email exists
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // validate password
+    const validPassword = await compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // generate access token
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    return res.json({ user, token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Something went wrong" });
