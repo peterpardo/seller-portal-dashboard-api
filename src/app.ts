5d32a86d4afb1a513f9116bdddadfb332602b902
Peter Pardo <peterpardo123@gmail.com>
2025-08-19 22:28:13 +0800

    Setup auth routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+app.use("/api/auth", authRoutes);
 
 export default app;
